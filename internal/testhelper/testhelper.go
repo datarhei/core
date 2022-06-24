a211de985041ddee5c2bb773c18de9ceb472186b
Ingo Oppermann <ingo@datarhei.com>
2022-06-24 19:49:53 +0200

    Remove print in tests

--- internal/testhelper/testhelper.go
+++ internal/testhelper/testhelper.go
@@ -10,8 +10,6 @@ func BuildBinary(name, pathprefix string) (string, error) {
 	dir := filepath.Join(pathprefix, name)
 	aout := filepath.Join(dir, name)
 
-	fmt.Printf("aout: %s\n", aout)
-
 	err := exec.Command("go", "build", "-o", aout, dir).Run()
 	if err != nil {
 		return "", fmt.Errorf("build command: %w", err)
