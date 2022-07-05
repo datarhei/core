4420d04a3b00e61a955c93d63c24823fdb6377a2
Ingo Oppermann <ingo@datarhei.com>
2022-07-05 18:52:54 +0200

    Add config option for RTMPS listen address

--- config/config.go
+++ config/config.go
@@ -129,11 +129,12 @@ type Data struct {
 		MimeTypes string `json:"mimetypes_file"`
 	} `json:"storage"`
 	RTMP struct {
-		Enable    bool   `json:"enable"`
-		EnableTLS bool   `json:"enable_tls"`
-		Address   string `json:"address"`
-		App       string `json:"app"`
-		Token     string `json:"token"`
+		Enable     bool   `json:"enable"`
+		EnableTLS  bool   `json:"enable_tls"`
+		Address    string `json:"address"`
+		AddressTLS string `json:"address_tls"`
+		App        string `json:"app"`
+		Token      string `json:"token"`
 	} `json:"rtmp"`
 	SRT struct {
 		Enable     bool   `json:"enable"`
@@ -349,6 +350,7 @@ func (d *Config) init() {
 	d.val(newBoolValue(&d.RTMP.Enable, false), "rtmp.enable", "CORE_RTMP_ENABLE", nil, "Enable RTMP server", false, false)
 	d.val(newBoolValue(&d.RTMP.EnableTLS, false), "rtmp.enable_tls", "CORE_RTMP_ENABLE_TLS", nil, "Enable RTMPS server instead of RTMP", false, false)
 	d.val(newAddressValue(&d.RTMP.Address, ":1935"), "rtmp.address", "CORE_RTMP_ADDRESS", nil, "RTMP server listen address", false, false)
+	d.val(newAddressValue(&d.RTMP.AddressTLS, ":1936"), "rtmp.address_tls", "CORE_RTMP_ADDRESS_TLS", nil, "RTMPS server listen address", false, false)
 	d.val(newStringValue(&d.RTMP.App, "/"), "rtmp.app", "CORE_RTMP_APP", nil, "RTMP app for publishing", false, false)
 	d.val(newStringValue(&d.RTMP.Token, ""), "rtmp.token", "CORE_RTMP_TOKEN", nil, "RTMP token for publishing and playing", false, true)
 
