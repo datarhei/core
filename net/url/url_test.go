8c0f2ebabc640e63f3e34fa71fd5edabeab9dc6b
Ingo Oppermann <ingo@datarhei.com>
2023-01-24 16:45:28 +0100

    Add tests

--- net/url/url_test.go
+++ net/url/url_test.go
@@ -7,9 +7,20 @@ import (
 )
 
 func TestLookup(t *testing.T) {
-	_, err := Lookup("https://www.google.com")
+	ip, err := Lookup("/localhost:8080/foobar")
 
 	require.NoError(t, err)
+	require.Equal(t, "", ip)
+
+	ip, err = Lookup("http://")
+
+	require.NoError(t, err)
+	require.Equal(t, "", ip)
+
+	ip, err = Lookup("https://www.google.com")
+
+	require.NoError(t, err)
+	require.NotEmpty(t, ip)
 }
 
 func TestLocalhost(t *testing.T) {
@@ -17,4 +28,23 @@ func TestLocalhost(t *testing.T) {
 
 	require.NoError(t, err)
 	require.Subset(t, []string{"127.0.0.1", "::1"}, []string{ip})
+}
+
+func TestValidate(t *testing.T) {
+	err := Validate("http://localhost/foobar")
+	require.NoError(t, err)
+
+	err = Validate("foobar")
+	require.NoError(t, err)
+}
+
+func TestScheme(t *testing.T) {
+	r := HasScheme("http://localhost/foobar")
+	require.True(t, r)
+
+	r = HasScheme("iueriherfd://localhost/foobar")
+	require.True(t, r)
+
+	r = HasScheme("//localhost/foobar")
+	require.False(t, r)
 }
