c05e16b6a0f8088a6701cc03f1d0122e66209134
Ingo Oppermann <ingo@datarhei.com>
2023-01-24 21:04:24 +0100

    Add tests

--- log/log_test.go
+++ log/log_test.go
@@ -5,15 +5,15 @@ import (
 	"bytes"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestLoglevelNames(t *testing.T) {
-	assert.Equal(t, "DEBUG", Ldebug.String())
-	assert.Equal(t, "ERROR", Lerror.String())
-	assert.Equal(t, "WARN", Lwarn.String())
-	assert.Equal(t, "INFO", Linfo.String())
-	assert.Equal(t, `SILENT`, Lsilent.String())
+	require.Equal(t, "DEBUG", Ldebug.String())
+	require.Equal(t, "ERROR", Lerror.String())
+	require.Equal(t, "WARN", Lwarn.String())
+	require.Equal(t, "INFO", Linfo.String())
+	require.Equal(t, `SILENT`, Lsilent.String())
 }
 
 func TestLogColorToNotTTY(t *testing.T) {
@@ -23,7 +23,7 @@ func TestLogColorToNotTTY(t *testing.T) {
 	w := NewConsoleWriter(writer, Linfo, true).(*syncWriter)
 	formatter := w.writer.(*consoleWriter).formatter.(*consoleFormatter)
 
-	assert.NotEqual(t, true, formatter.color, "Color should not be used on a buffer logger")
+	require.NotEqual(t, true, formatter.color, "Color should not be used on a buffer logger")
 }
 
 func TestLogContext(t *testing.T) {
@@ -53,7 +53,7 @@ func TestLogContext(t *testing.T) {
 	lenWithoutCtx := buffer.Len()
 	buffer.Reset()
 
-	assert.Greater(t, lenWithCtx, lenWithoutCtx, "Log line length without context is not shorter than with context")
+	require.Greater(t, lenWithCtx, lenWithoutCtx, "Log line length without context is not shorter than with context")
 }
 
 func TestLogClone(t *testing.T) {
@@ -65,7 +65,7 @@ func TestLogClone(t *testing.T) {
 	logger.Info().Log("info")
 	writer.Flush()
 
-	assert.Contains(t, buffer.String(), `component="test"`)
+	require.Contains(t, buffer.String(), `component="test"`)
 
 	buffer.Reset()
 
@@ -74,7 +74,7 @@ func TestLogClone(t *testing.T) {
 	logger2.Info().Log("info")
 	writer.Flush()
 
-	assert.Contains(t, buffer.String(), `component="tset"`)
+	require.Contains(t, buffer.String(), `component="tset"`)
 }
 
 func TestLogSilent(t *testing.T) {
@@ -85,22 +85,22 @@ func TestLogSilent(t *testing.T) {
 
 	logger.Debug().Log("debug")
 	writer.Flush()
-	assert.Equal(t, 0, buffer.Len(), "Buffer should be empty")
+	require.Equal(t, 0, buffer.Len(), "Buffer should be empty")
 	buffer.Reset()
 
 	logger.Info().Log("info")
 	writer.Flush()
-	assert.Equal(t, 0, buffer.Len(), "Buffer should be empty")
+	require.Equal(t, 0, buffer.Len(), "Buffer should be empty")
 	buffer.Reset()
 
 	logger.Warn().Log("warn")
 	writer.Flush()
-	assert.Equal(t, 0, buffer.Len(), "Buffer should be empty")
+	require.Equal(t, 0, buffer.Len(), "Buffer should be empty")
 	buffer.Reset()
 
 	logger.Error().Log("error")
 	writer.Flush()
-	assert.Equal(t, 0, buffer.Len(), "Buffer should be empty")
+	require.Equal(t, 0, buffer.Len(), "Buffer should be empty")
 	buffer.Reset()
 }
 
@@ -112,22 +112,22 @@ func TestLogDebug(t *testing.T) {
 
 	logger.Debug().Log("debug")
 	writer.Flush()
-	assert.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
+	require.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
 	buffer.Reset()
 
 	logger.Info().Log("info")
 	writer.Flush()
-	assert.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
+	require.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
 	buffer.Reset()
 
 	logger.Warn().Log("warn")
 	writer.Flush()
-	assert.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
+	require.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
 	buffer.Reset()
 
 	logger.Error().Log("error")
 	writer.Flush()
-	assert.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
+	require.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
 	buffer.Reset()
 }
 
@@ -139,22 +139,22 @@ func TestLogInfo(t *testing.T) {
 
 	logger.Debug().Log("debug")
 	writer.Flush()
-	assert.Equal(t, 0, buffer.Len(), "Buffer should be empty")
+	require.Equal(t, 0, buffer.Len(), "Buffer should be empty")
 	buffer.Reset()
 
 	logger.Info().Log("info")
 	writer.Flush()
-	assert.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
+	require.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
 	buffer.Reset()
 
 	logger.Warn().Log("warn")
 	writer.Flush()
-	assert.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
+	require.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
 	buffer.Reset()
 
 	logger.Error().Log("error")
 	writer.Flush()
-	assert.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
+	require.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
 	buffer.Reset()
 }
 
@@ -166,22 +166,22 @@ func TestLogWarn(t *testing.T) {
 
 	logger.Debug().Log("debug")
 	writer.Flush()
-	assert.Equal(t, 0, buffer.Len(), "Buffer should be empty")
+	require.Equal(t, 0, buffer.Len(), "Buffer should be empty")
 	buffer.Reset()
 
 	logger.Info().Log("info")
 	writer.Flush()
-	assert.Equal(t, 0, buffer.Len(), "Buffer should be empty")
+	require.Equal(t, 0, buffer.Len(), "Buffer should be empty")
 	buffer.Reset()
 
 	logger.Warn().Log("warn")
 	writer.Flush()
-	assert.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
+	require.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
 	buffer.Reset()
 
 	logger.Error().Log("error")
 	writer.Flush()
-	assert.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
+	require.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
 	buffer.Reset()
 }
 
@@ -193,21 +193,43 @@ func TestLogError(t *testing.T) {
 
 	logger.Debug().Log("debug")
 	writer.Flush()
-	assert.Equal(t, 0, buffer.Len(), "Buffer should be empty")
+	require.Equal(t, 0, buffer.Len(), "Buffer should be empty")
 	buffer.Reset()
 
 	logger.Info().Log("info")
 	writer.Flush()
-	assert.Equal(t, 0, buffer.Len(), "Buffer should be empty")
+	require.Equal(t, 0, buffer.Len(), "Buffer should be empty")
 	buffer.Reset()
 
 	logger.Warn().Log("warn")
 	writer.Flush()
-	assert.Equal(t, 0, buffer.Len(), "Buffer should be empty")
+	require.Equal(t, 0, buffer.Len(), "Buffer should be empty")
 	buffer.Reset()
 
 	logger.Error().Log("error")
 	writer.Flush()
-	assert.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
+	require.NotEqual(t, 0, buffer.Len(), "Buffer should not be empty")
 	buffer.Reset()
+}
+
+func TestLogWithField(t *testing.T) {
+	bufwriter := NewBufferWriter(Linfo, 10)
+
+	logger := New("test").WithOutput(bufwriter)
+	logger = logger.WithField("foo", "bar")
+	logger.Info().Log("hello")
+
+	events := bufwriter.Events()
+
+	require.Equal(t, 1, len(events))
+	require.Empty(t, events[0].err)
+	require.Equal(t, "bar", events[0].Data["foo"])
+
+	logger = logger.WithField("func", func() bool { return true })
+	logger.Info().Log("hello")
+
+	events = bufwriter.Events()
+	require.Equal(t, 2, len(events))
+	require.NotEmpty(t, events[1].err)
+	require.Equal(t, "bar", events[0].Data["foo"])
 }
