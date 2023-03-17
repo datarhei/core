562b7aed922159ff5462aac81355533a5776b9b6
Ingo Oppermann <ingo@datarhei.com>
2023-03-17 13:55:19 +0100

    Use better naming for storage endpoint documentation

--- http/handler/api/filesystems.go
+++ http/handler/api/filesystems.go
@@ -31,16 +31,17 @@ func NewFS(filesystems map[string]FSConfig) *FSHandler {
 // GetFileAPI returns the file at the given path
 // @Summary Fetch a file from a filesystem
 // @Description Fetch a file from a filesystem
+// @Tags v16.7.2
 // @ID filesystem-3-get-file
 // @Produce application/data
 // @Produce json
-// @Param name path string true "Name of the filesystem"
-// @Param path path string true "Path to file"
+// @Param storage path string true "Name of the filesystem"
+// @Param filepath path string true "Path to file"
 // @Success 200 {file} byte
 // @Success 301 {string} string
 // @Failure 404 {object} api.Error
 // @Security ApiKeyAuth
-// @Router /api/v3/fs/{name}/{path} [get]
+// @Router /api/v3/fs/{storage}/{filepath} [get]
 func (h *FSHandler) GetFile(c echo.Context) error {
 	name := util.PathParam(c, "name")
 
@@ -55,18 +56,19 @@ func (h *FSHandler) GetFile(c echo.Context) error {
 // PutFileAPI adds or overwrites a file at the given path
 // @Summary Add a file to a filesystem
 // @Description Writes or overwrites a file on a filesystem
+// @Tags v16.7.2
 // @ID filesystem-3-put-file
 // @Accept application/data
 // @Produce text/plain
 // @Produce json
-// @Param name path string true "Name of the filesystem"
-// @Param path path string true "Path to file"
+// @Param storage path string true "Name of the filesystem"
+// @Param filepath path string true "Path to file"
 // @Param data body []byte true "File data"
 // @Success 201 {string} string
 // @Success 204 {string} string
 // @Failure 507 {object} api.Error
 // @Security ApiKeyAuth
-// @Router /api/v3/fs/{name}/{path} [put]
+// @Router /api/v3/fs/{storage}/{filepath} [put]
 func (h *FSHandler) PutFile(c echo.Context) error {
 	name := util.PathParam(c, "name")
 
@@ -81,14 +83,15 @@ func (h *FSHandler) PutFile(c echo.Context) error {
 // DeleteFileAPI removes a file from a filesystem
 // @Summary Remove a file from a filesystem
 // @Description Remove a file from a filesystem
+// @Tags v16.7.2
 // @ID filesystem-3-delete-file
 // @Produce text/plain
-// @Param name path string true "Name of the filesystem"
-// @Param path path string true "Path to file"
+// @Param storage path string true "Name of the filesystem"
+// @Param filepath path string true "Path to file"
 // @Success 200 {string} string
 // @Failure 404 {object} api.Error
 // @Security ApiKeyAuth
-// @Router /api/v3/fs/{name}/{path} [delete]
+// @Router /api/v3/fs/{storage}/{filepath} [delete]
 func (h *FSHandler) DeleteFile(c echo.Context) error {
 	name := util.PathParam(c, "name")
 
@@ -103,15 +106,16 @@ func (h *FSHandler) DeleteFile(c echo.Context) error {
 // ListFiles lists all files on a filesystem
 // @Summary List all files on a filesystem
 // @Description List all files on a filesystem. The listing can be ordered by name, size, or date of last modification in ascending or descending order.
+// @Tags v16.7.2
 // @ID filesystem-3-list-files
 // @Produce json
-// @Param name path string true "Name of the filesystem"
+// @Param storage path string true "Name of the filesystem"
 // @Param glob query string false "glob pattern for file names"
 // @Param sort query string false "none, name, size, lastmod"
 // @Param order query string false "asc, desc"
 // @Success 200 {array} api.FileInfo
 // @Security ApiKeyAuth
-// @Router /api/v3/fs/{name} [get]
+// @Router /api/v3/fs/{storage} [get]
 func (h *FSHandler) ListFiles(c echo.Context) error {
 	name := util.PathParam(c, "name")
 
@@ -126,6 +130,7 @@ func (h *FSHandler) ListFiles(c echo.Context) error {
 // List lists all registered filesystems
 // @Summary List all registered filesystems
 // @Description Listall registered filesystems
+// @Tags v16.12.0
 // @ID filesystem-3-list
 // @Produce json
 // @Success 200 {array} api.FilesystemInfo
